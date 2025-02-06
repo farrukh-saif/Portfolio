28c5c9dfde293682bc94da1ea45c0605792052f5
Farrukh Saif <syedfarrukhsaif@gmail.com>
2025-02-05 21:13:32 -0500

    Remove useless tools

--- app/page.tsx
+++ app/page.tsx
@@ -24,10 +24,10 @@ export default function Page() {
           priority
         />
       </a>
-
+{/* 
       <div className="prose prose-neutral dark:prose-invert">
       <p>⚠️ I'm building out the narration feature and yes I'm testing on production. It's my website boi I does what I wish to. 🚧 </p>
-      </div>
+      </div> */}
 
       <h1 className="text-2xl font-medium tracking-tight">
         Hey there! I'm Farrukh 👋
@@ -49,11 +49,19 @@ export default function Page() {
         <p>
           Just a regular dude who's into tech and is tryna have a fun time on Earth. Did my undergrad in <strong>Computer Engineering at ODTÜ</strong> in Turkey. Moved to <strong>Canada {Math.floor((-new Date('2024-09-06').getTime() + new Date().getTime()) / (1000 * 60 * 60 * 24 * 30))} months ago</strong>. Starting my <strong>Master’s in AI at the University of Waterloo</strong> this Spring. My experience spans <strong>machine learning, software development, and augmented reality</strong>. The type of nerd who reads research papers for fun and builds because curiosity won’t let me do otherwise.
         </p>
-        <div className="flex flex-wrap justify-center items-center gap-4">
-          <img className="h-11.5 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=py,pytorch,sklearn,selenium,anaconda,unity" alt="My Skills" />
-          <img className="h-11.5 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=tensorflow,opencv,flutter,firebase,react,androidstudio" alt="My Skills" />
-          <img className="h-11.5 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=cs,docker,gcp,vscode,git,postman" alt="My Skills" />
-          <img className="h-11.5 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=c,cpp,arduino,npm,pnpm,mysql" alt="My Skills" />
+        
+      </div>
+
+      <div className="flex flex-col sm:flex-row sm:items-center gap-3 mb-8 mt-6">
+          <h1 className="text-2xl font-medium tracking-tight">
+            Tools of the trade
+          </h1>
+        </div>
+        <div className="prose prose-neutral dark:prose-invert">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <img className="h-10 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=py,pytorch,sklearn,selenium,anaconda,unity" alt="My Skills" />
+          <img className="h-10 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=tensorflow,opencv,flutter,firebase,react,androidstudio" alt="My Skills" />
+          <img className="h-10 grayscale-[0.5] hover:grayscale-[0]" src="https://skillicons.dev/icons?i=cs,docker,gcp,arduino,git,mysql" alt="My Skills" />
         </div>
         <p className="text-center text-sm italic">
           These might be tools I've used extensively in the past, but believe me when I say that my tools are just that, my tools. It's the problem that matters, I'll learn whatever it takes.
